39b2e8859d3ec3e8adee8903bbd9508bc2f3901c
raymord <reymordn@nineenthamendment.com>
2016-08-10 17:03:00 -0400

    I did something

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+
+        <!DOCTYPE html>
+    <html>
+        <body>
+
+            <?php
+            $servername = "localhost";
+            $username = "reymord";
+            $password = "dtycts16";
+            $dbname = "test";
+
+// Create connection
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            $sql = "SELECT title, author, publisher, year, ISBN, price, quantity FROM final";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                // output data of each row
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "Information" . ' ' . $row["title"]  . ' '. $row["author"]  . ' '. $row["publisher"] . ' '. $row["year"] . ' '. $row["ISBN"] . $row["price"]. ' '. $row["quantity"]."<br>";
+                } 
+            } else {
+                echo "0 results";
+            }
+
+            mysqli_close($conn);
+            ?>  
+
+        </body>
+    </html>
+
